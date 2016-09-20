671bf62d5297f825a1ab74e8ebd79613784e85ac
bioinfornatics <bioinfornatics@fedoraproject.org>
2016-09-20 13:33:31 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,36 +1,67 @@
 #!/usr/bin/env bash
-declare -ar projects=(grools-reasoner bio-scribe grools-genome-properties grools-obo grools-reporter)
+die () {
+    local parent_lineno message code
+    parent_lineno="$1"
+    message="$2"
+    [[ -n $3 ]] && code="$3" || code=1
+    PATH=$OLDPATH
+    if [[ -n "$message" ]] ; then
+        echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}" >&2
+    else
+        echo "Error on or near line ${parent_lineno}; exiting with status ${code}" >&2
+    fi
+    exit "${code}"
+}
+
+trap 'die ${LINENO}' 1 15 ERR
+
+declare -ar projects=(Grools/grools-reasoner institut-de-genomique/bio-scribe Grools/grools-genome-properties-plugins Grools/grools-obo-plugins Grools/grools-reporter)
 
 command -v dot >/dev/null 2>&1 || { echo >&2 "I require dot but it's not installed.  Aborting."; exit 1; }
 runner="gradle"
-if command -v "${runner}"  2>&1; then
+
+echo '============='
+if command -v "${runner}"  2>&1 >/dev/null; then
     echo 'Using gradle'
 else
     runner='./gradlew'
     echo 'Using gradlew'
 fi
+echo '============='
+echo ''
 
-mkdir build
-push build
-    git clone https://github.com/Grools/grools-reasoner
-    git clone https://github.com/institut-de-genomique/bio-scribe
-    git clone https://github.com/Grools/grools-genome-properties-pugins
-    git clone https://github.com/Grools/grools-obo-pugins
-    git clone https://github.com/Grools/grools-reporter
-
+[[ ! -e build/dependencies ]] && mkdir -p build/dependencies
+pushd build/dependencies > /dev/null
     for project in "${projects[@]}"; do
-        pushd ${project}
-          ${runner} clean build install
-        popd
-    done
-popd
+        projectname="${project#*/}"
+        msg='Installing '"${projectname}"
 
-gradle clean shadowJar
+        echo "${msg}"
+        printf '*%.0s' $(seq  1 ${#msg})
+        echo ''
+
+        if [[ ! -d "${projectname}" ]]; then
+            git clone --quiet https://github.com/"${project}".git "${projectname}" 
+        else
+            ( pushd ${projectname}; git pull; popd ) > /dev/null
+        fi
+        pushd ${projectname} > /dev/null
+          ${runner} -q clean build install
+        popd > /dev/null
+
+        echo ''
+    done
+popd > /dev/null
+
+${runner} -q shadowJar
 
 declare -ra jars=(build/libs/*.jar)
 
+echo ''
+echo '============================================================================================'
 echo 'Generated files:'
 
 for j in "${jars[@]]}"; do
     echo "  - $j"
 done
+echo '============================================================================================'
